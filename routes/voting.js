19e7e6b8c02b1bc5f3f2bec7920c2aec4974c113
philipthedeveloper <philipowolabi79@gmail.com>
2023-07-22 17:46:21 +0100

    Request access to see results

--- routes/voting.js
+++ routes/voting.js
@@ -10,6 +10,6 @@ const votingRouter = require("express").Router();
 votingRouter.route("/get-contestants").get(getContestant);
 votingRouter.route("/submit-vote").post(checkAccess, submitVote);
 votingRouter.route("/get-result").post(checkAccess, getResult);
-votingRouter.route("/results").get(allResult);
+votingRouter.route("/results").get(checkAccess, allResult);
 
 module.exports = votingRouter;
